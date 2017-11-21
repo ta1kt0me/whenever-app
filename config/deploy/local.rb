f7eb8f14aaa054226a1c49fde2e114dbb1e7ce80
ta1kt0me <p.wadachi@gmail.com>
2017-11-21 21:27:01 +0900

    Deploy from github to vagrant machine

---
+++ config/deploy/local.rb
@@ -0,0 +1,9 @@
+server "127.0.0.1", user: "vagrant"
+
+set :ssh_options, {
+  user: 'vagrant',
+  keys: "#{File.expand_path(File.dirname(__FILE__) + '../../../')}/.vagrant/machines/default/virtualbox/private_key",
+  forward_agent: true,
+  auth_methods: %w(publickey password),
+  port: 2222
+}
